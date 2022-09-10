7be9f34eb99ca1a079055845034537b53a185aad
Bhavesh Patil <dadu.patil@gmail.com>
2022-09-10 19:39:37 +0530

    removed simulation from the collection

--- sim/item.py
+++
@@ -1,9 +0,0 @@
-import string
-
-class Item():
-    def __init__(self, name, ticker, category, weight, volume) -> None:
-        self.name: string = name
-        self.ticker: string = ticker
-        self.category: string = category
-        self.weight: float = weight
-        self.volume: float = volume
