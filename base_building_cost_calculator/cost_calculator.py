c016464a2dc13a726f1f5de67f90061b898d00ae
Bhavesh Patil <dadu.patil@gmail.com>
2022-10-01 16:01:02 +0530

    better import, ticker input with check

--- base_building_cost_calculator/cost_calculator.py
+++ base_building_cost_calculator/cost_calculator.py
@@ -3,15 +3,58 @@ import sys
 import requests
 import json
 
-def get_new_data():
-    response = requests.get('https://rest.fnar.net/building/allbuildings')
-    with open(os.path.join(sys.path[0], './buildings.json'), 'w') as jsonf:
-        json_data = json.loads(response.text)
-        jsonf.write(json.dumps(json_data, indent=4))
-    print("New Data Imported")
+
+def get_old_data(file_path):
+    with open(file_path, 'r') as jsonf:
+        data = json.load(jsonf)
+        return data
+
+def get_new_data(file_path):
+    try:
+        response = requests.get('https://rest.fnar.net/building/allbuildings')
+        with open(file_path, 'w') as jsonf:
+            json_data = {"buildings": json.loads(response.text)}
+            jsonf.write(json.dumps(json_data, indent=4))
+            print("New Data Imported")
+            return json_data
+    except ConnectionError:
+        print("Internet Issue! Could not grab data. Using existing data.")
+        return get_old_data(file_path)
+
+def input_buildings(buildings, data):
+    ticker = input("Building Ticker?: ")
     
+    if ticker == '':
+        print("Please input a building name.")
+        return
+
+    for item in data["buildings"]:
+        if item["Ticker"] == ticker:
+            amount = input("Build Amount? (default: 1): ") or 1
+            buildings.append({ "building": item, "amount": amount })
+            print(f"Building Added: {item['Name']}, {amount}x.")
+            return
+    
+    print("Bad Ticker.")
+
 if __name__ == "__main__":
-    get_data = input("Import new data? (y/n) (Default: y): ") or 'y'
-    if get_data == 'y':
+    file_path = os.path.join(sys.path[0], './buildings.json')
+    get_data = input("Import new data? (y/n) (default: y): ") or 'y'
+    
+    if get_data == 'y' or not os.path.isfile(file_path):
         print("Importing new data.")
-        get_new_data()
+        data = get_new_data(file_path)
+    else:
+        data = get_old_data(file_path)
+
+    buildings = []
+    is_done = False
+
+    while not is_done:
+        input_buildings(buildings, data)
+    
+        is_done_input = input('Done? (y/n) (default: n): ')
+        if is_done_input == 'y':
+            is_done = True
+    
+    print(buildings)
